3a48a0c51b5e32aa20cb3fc512584b834b20d4d9
Apothecary1995 <ahmet.cengiz19951995@gmail.com>
2026-05-07 22:45:50 +0200

    feat: first initilazition for devops and export worker test and function for connection

---
+++ workers/export_worker/main_test.go
@@ -0,0 +1,20 @@
+package main 
+
+
+import (
+
+	"os"
+	"testing"
+)
+func TestConnectRabbitMQ_InvalidURL(t *testing.T) {
+	// Geçersiz URL ile bağlantı hata vermeli
+	_, err := connectRabbitMQ("amqp://invalid:5672/")
+
+	if err == nil {
+		t.Error("geçersiz URL için hata bekliyordu")
+	}
+}
+//will add geturl test needed
+func main(){
+
+}
