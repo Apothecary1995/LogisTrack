d1a949fb8683dabde2435d9322cc57d6ca4cb49c
Apothecary1995 <104367082+Apothecary1995@users.noreply.github.com>
2026-05-20 21:33:12 +0200

    chore: import for publiser file

--- LogisTrack/backend/api/views.py
+++ LogisTrack/backend/api/views.py
@@ -14,7 +14,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from .publisher import publish_notification
+from .publisher import publish_notification, publish_export
 from .models import (
     DriverLeave,
     DriverProfile,
