d49b11605c5f4f28f3886258ade56b2f5bf68bab
Apothecary1995 <104367082+Apothecary1995@users.noreply.github.com>
2026-05-21 14:54:40 +0200

    fix: vite-config updated for pouchdb

--- LogisTrack/frontend/vite.config.js
+++ LogisTrack/frontend/vite.config.js
@@ -33,12 +33,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/favicon.png",
+            src: "/icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/favicon.png",
+            src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
@@ -46,6 +46,9 @@ export default defineConfig({
       },
     }),
   ],
+  optimizeDeps: {
+    include: ["pouchdb", "pouchdb-find"],
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
